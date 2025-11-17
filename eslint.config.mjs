a416a39963c185fc2543c6723d85401c3373b9bf
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-16 22:39:52 -0300

    feat: install and configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,29 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   {
     rules: {
-      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
-      "react/prop-types": "off", // Since we use TypeScript for type checking
-      "react/react-in-jsx-scope": "off", // Not needed with Next.js
-      "@typescript-eslint/explicit-module-boundary-types": "off", // Allow implicit return types
-    },
+      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+      'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+      'react/prop-types': 'off', // Since we use TypeScript for type checking
+      'react/react-in-jsx-scope': 'off', // Not needed with Next.js
+      '@typescript-eslint/explicit-module-boundary-types': 'off' // Allow implicit return types
+    }
   },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
+  ])
+])
 
-export default eslintConfig;
+export default eslintConfig
