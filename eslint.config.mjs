722b8a02ff7fccb0c57c8255de48c964d23eba40
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-16 21:25:58 -0300

    feat: set eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+      "react/prop-types": "off", // Since we use TypeScript for type checking
+      "react/react-in-jsx-scope": "off", // Not needed with Next.js
+      "@typescript-eslint/explicit-module-boundary-types": "off", // Allow implicit return types
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
