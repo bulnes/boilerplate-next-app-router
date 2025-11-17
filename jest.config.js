43ac2e8f9e83ebb39e800221a037214c11ec12e3
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-17 10:28:45 -0300

    feat: install and configure jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const jestConfig = {
+  testEnvironment: 'jsdom', // Browser environment for testing React components
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], // Ignore node_modules and .next directories
+  collectCoverage: true, // Enable code coverage collection
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?' // Collect coverage from all source files
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Setup file for additional configurations
+  modulePaths: ['<rootDir>/src/'], // Root directory for module resolution
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }] // Use babel-jest for transforming files
+  }
+}
+
+export default jestConfig
