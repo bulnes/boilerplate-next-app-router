e31d8e20ce8341a57774cb16d6167cb7656123cb
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-23 18:51:59 -0300

    feat: public first custom story on storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,30 @@
-function Main() {
+import Image from 'next/image'
+
+function Main({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e TailwindCSS'
+}) {
   return (
-    <main className="h-full flex items-center justify-center bg-purple-900 text-white">
-      <h1 className="text-4xl font-bold">React Avançado</h1>
+    <main className="bg-[#06092b] text-white w-full h-full p-12 text-center flex flex-col items-center justify-center">
+      <Image
+        src="/img/logo.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        className=""
+      />
+
+      <h1 className="text-4xl">{title}</h1>
+
+      <h2 className="text-xl font-normal">{description}</h2>
+
+      <Image
+        src="/img/hero-illustration.svg"
+        alt="Hero Illustration"
+        width={300}
+        height={100}
+        className="mt-12 w-[min(30rem, 100%)]"
+      />
     </main>
   )
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -12,3 +12,10 @@ const meta: Meta = {
 export default meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'Boilerplate Next App Router',
+    description: 'TypeScript, ReactJS, NextJS e TailwindCSS'
+  }
+}
