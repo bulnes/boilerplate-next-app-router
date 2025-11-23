5d6e3ee198713ca65459c29e4492111b68bb659b
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-23 01:48:11 -0300

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ const jestConfig = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], // Ignore node_modules and .next directories
   collectCoverage: true, // Enable code coverage collection
   collectCoverageFrom: [
-    'src/**/*.ts(x)?' // Collect coverage from all source files
+    'src/**/*.ts(x)?', // Collect coverage from all source files
+    '!src/app/**' // Exclude app directory
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Setup file for additional configurations
   modulePaths: ['<rootDir>/src/'], // Root directory for module resolution
