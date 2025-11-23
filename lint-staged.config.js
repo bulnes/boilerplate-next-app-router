a5ade5a052f9fd4a534a14ac32b33948eacec5d8
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-23 01:20:17 -0300

    feat: add jest configs and add first test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 const lintStagedConfig = {
   '*.{js,jsx,ts,tsx}': (stagedFiles) => [
     `prettier --write ${stagedFiles.join(' ')}`,
-    `eslint --fix ${stagedFiles.join(' ')}`
+    `eslint --fix ${stagedFiles.join(' ')}`,
+    `jest --findRelatedTests ${stagedFiles.join(' ')}`
   ]
 }
 
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should be defined', () => {
+    expect(Main).toBeDefined()
+  })
+
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
